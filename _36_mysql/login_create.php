253e484a14f355e45fe2d9d22f613f2189ebe921
John (Jef) Fisher <Fishe545@gmail.com>
2017-09-09 11:46:03 -0700

    read functionality is complete

--- _36_mysql/login_create.php
+++ _36_mysql/login_create.php
@@ -1,4 +1,5 @@
 <?php
+
 if (isset($_POST['submit'])) {
 
     $username = $_POST['username'];
@@ -38,7 +39,7 @@ if (isset($_POST['submit'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Form</title>
+    <title>Login Create</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
           integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 </head>
