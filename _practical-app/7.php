9a5282cdc8be437301a2ae63d1ac835c4966954a
John (Jef) Fisher <Fishe545@gmail.com>
2017-09-10 12:39:16 -0700

    connected to db

--- _practical-app/7.php
+++ _practical-app/7.php
@@ -1,39 +1,55 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
-    
 
-	<section class="content">
+<?php
+$conn = mysqli_connect('localhost', 'root', 'root', 'pp_seven');
 
-		<aside class="col-xs-4">
-
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
-
-
-	<article class="main-content col-xs-8">
-	
-	
-	
-	<?php  
-
-	/*  Step 1 - Create a database in PHPmyadmin
-
-		Step 2 - Create a table like the one from the lecture
-
-		Step 3 - Insert some Data
-
-		Step 4 - Connect to Database and read data
-
-*/
-	
-	?>
+$status = (function ()
+{
+    global $conn;
+    if (!$conn) {
+        return die('DB connect failed: ' . mysqli_error($conn));
+    }
+})();
+?>
 
 
+<section class="content">
+
+    <aside class="col-xs-4">
+
+        <?php Navigation(); ?>
 
 
+    </aside><!--SIDEBAR-->
 
-</article><!--MAIN CONTENT-->
 
-<?php include "includes/footer.php" ?>
+    <article class="main-content col-xs-8">
+
+
+        <?php
+
+        /*  Step 1 - Create a database in PHPmyadmin
+
+            Step 2 - Create a table like the one from the lecture
+
+            Step 3 - Insert some Data
+
+            Step 4 - Connect to Database and read data
+
+    */
+        ?>
+        <div class="row">
+            <div class="col-sm-12">
+                <?= $status; ?>
+                <pre>
+
+                </pre>
+
+            </div>
+        </div>
+
+
+    </article><!--MAIN CONTENT-->
+
+    <?php include "includes/footer.php" ?>
